c9e71c616db0d5dc03439fd69dcbb2ee158cc43a
Brayan Garcia <bgarcia.95@outlook.com>
2024-09-02 16:42:07 -0600

    Initial commit

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,13 @@
+import {View} from 'react-native';
+import React from 'react';
+import {ReusableButton} from 'common-components-1/components/ResusableButton';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ReusableButton />
+    </View>
+  );
+};
+
+export default App;
